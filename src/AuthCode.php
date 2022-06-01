072fa0379c0ac13a4d9b04f2f2cb3c08f50a4a1e
doyu2020 <dybee89@gmail.com>
2022-06-01 17:27:32 +0800

    commit for mark change

--- src/AuthCode.php
+++ src/AuthCode.php
@@ -8,11 +8,11 @@ class AuthCode
     /** @var string session键值 */
     const SESSION_USER_NAME = 'user_session';
     /** @var int 系统异常代码 */
-    const SERVER_ERROR = 500;
+    const SERVER_ERROR = 5000;
     /** @var int 登陆异常代码 */
-    const LOGIN_FAILED = 400;
+    const LOGIN_FAILED = 4000;
     /** @var int 业务异常代码 */
-    const BUSINESS_ERROR = 300;
+    const BUSINESS_ERROR = 3000;
     /** @var int 无权限错误 */
-    const AUTH_ERROR = 403;
+    const AUTH_ERROR = 4003;
 }
