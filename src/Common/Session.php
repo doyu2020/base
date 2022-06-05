6ac633040573e7d35c735376fd8714b5fe5a18e6
doyu2020 <dybee89@gmail.com>
2022-06-06 01:37:14 +0800

    commit for mark change

--- src/Common/Session.php
+++ src/Common/Session.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
+
 namespace Dybee\Base\Common;
 
 use Dybee\Base\AuthCode;
@@ -17,13 +18,13 @@ use Hyperf\Context\Context;
 use Hyperf\Contract\SessionInterface;
 
 /**
- * @property int $id 用户ID
+ * @property int    $id 用户ID
  * @property string $user_name 用户名
- * @property int $user_access 用户权限 1-基础权限 2-客户权限 3-运营权限 4-超级权限
- * @property bool $canAdmin 是否有超级管理员权限
- * @property bool $canOperation 是否有运营权限
- * @property bool $canUser 是否有客户权限
- * @property bool $canDefault 是否有普通权限
+ * @property int    $user_access 用户权限 1-基础权限 2-客户权限 3-运营权限 4-超级权限
+ * @property bool   $canAdmin 是否有超级管理员权限
+ * @property bool   $canOperation 是否有运营权限
+ * @property bool   $canUser 是否有客户权限
+ * @property bool   $canDefault 是否有普通权限
  */
 class Session
 {
@@ -34,7 +35,7 @@ class Session
     public function __get($name)
     {
         $user = Context::get(AuthCode::SESSION_USER_NAME);
-        if (! $user) {
+        if (!$user) {
             throw new AuthException('请先登陆', AuthCode::LOGIN_FAILED);
         }
         return $user[$name] ?? throw new AuthException(sprintf('USER_SESSION_NAME[%s]不存在', $name));
@@ -58,5 +59,10 @@ class Session
     public function getUser(): array
     {
         return Context::get(AuthCode::SESSION_USER_NAME, []);
+    }
+
+    public function setUser(array $data)
+    {
+        $this->session->set(AuthCode::SESSION_USER_NAME, $data);
     }
 }
